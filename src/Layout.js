7a366b90efbeb5e1518795e6505fbe8fcd52a00d
Arnav Badrish <arnavbadrish@gmail.com>
2025-03-10 11:08:50 +1100

    Fixed the issue of button to expand the header buttons not working on mobile

--- src/Layout.js
+++ src/Layout.js
@@ -1,17 +1,24 @@
 import { Outlet, Link } from "react-router-dom";
+import { useState } from "react";
 
 export function Layout() {
+  const [isNavbarExpanded, setIsNavbarExpanded] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsNavbarExpanded(!isNavbarExpanded);
+  };
+
   return (
     <div className="max-w-full bg-slate-50 text-black">
       <header>
         <nav className="flex items-center justify-between flex-wrap bg-sky-400 p-6 rounded-b-3xl fixed w-screen top-0 shadow">
-          <div className="flex items-center flex-shrink-0 mr-6">
+          <div className="flex items-center flex-shrink-0 md:mr-6">
             <Link className="-m-1.5 p-1.5 font-semibold text-xl tracking-tight" to="/">Ar-196</Link>
           </div>
-          <button type="button" data-collapse-toggle = "navbar-collapsible" aria-controls = "navbar-collapsible" aria-expanded = "false" className="md:hidden text-white">
+          <button type="button" onClick={toggleNavbar} aria-expanded = {isNavbarExpanded} className="md:hidden text-white">
             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
           </button>
-          <div id="navbar-collapsible" className="w-full block flex-grow md:flex lg:items-center md:w-auto hidden">
+          <div id="navbar-collapsible" className={`w-full block flex-grow md:flex lg:items-center md:w-auto ${isNavbarExpanded ? '' : 'hidden'}`}>
             <div className="text-sm lg:flex-grow">
               <ul className="md:flex items-center justify-left">
                 <HeaderButton href="/projects" displayText="Projects" />
@@ -21,7 +28,7 @@ export function Layout() {
         </nav>
       </header>
 
-      <div class="pt-20"/>
+      <div className="pt-20"/>
 
       <Outlet/>
     </div>
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -3,7 +3,7 @@ function AboutMe() {
     <div id="about-me">
       <div className="border-2 p-10 md:p-15 flex flex-col justify-center items-center w-3/4 mx-auto">
         <p className="text-3xl"> 
-          Welcome to my site! 
+          Welcome to my site!
         </p>
         <p className="pt-6 pb-3">
           Hello, my name is Ar-196, and this is my website, which I made mainly 
