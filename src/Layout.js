9ee5760f265216beb6f8f758420f2ee1f2f803d9
Arnav Badrish <arnavbadrish@gmail.com>
2025-03-16 12:18:35 +1100

    Tweaked the header to collapse navbar when clicking one of the header buttons (small screens only)

--- src/Layout.js
+++ src/Layout.js
@@ -8,12 +8,21 @@ export function Layout() {
     setIsNavbarExpanded(!isNavbarExpanded);
   };
 
+  const disableNavbar = () => {
+    setIsNavbarExpanded(false);
+  };
+
+  const HeaderButton = ({ href, displayText }) =>
+    <li className="mr-6 hover:text-white">
+      <Link to={href} onClick={disableNavbar}>{displayText}</Link>
+    </li>
+
   return (
     <div className="max-w-full bg-slate-50 text-black">
       <header>
         <nav className="flex items-center justify-between flex-wrap bg-sky-400 p-6 rounded-b-3xl fixed w-screen top-0 shadow">
           <div className="flex items-center flex-shrink-0 md:mr-6">
-            <Link className="-m-1.5 p-1.5 font-semibold text-xl tracking-tight" to="/">Ar-196</Link>
+            <Link className="-m-1.5 p-1.5 font-semibold text-xl tracking-tight" to="/" onClick={disableNavbar}>Ar-196</Link>
           </div>
           <button type="button" onClick={toggleNavbar} aria-expanded = {isNavbarExpanded} className="md:hidden text-white">
             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
@@ -32,13 +41,5 @@ export function Layout() {
 
       <Outlet/>
     </div>
-  );
-}
-
-function HeaderButton({ href, displayText }) {
-  return (
-    <li className="mr-6 hover:text-white">
-        <Link to={href}>{displayText}</Link>
-    </li>
   );
 }
